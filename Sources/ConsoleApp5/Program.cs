76398f14a0de97e8e837a577790c8517cf779459
Ali MAKRI <github@makri.fr>
2024-03-04 17:14:36 +0100

    17:14

---
+++ Sources/ConsoleApp5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class O1
+    {
+        public void M1(int a)
+        {
+            Thread.Sleep(1000);
+            Console.WriteLine("Méthode M1 de la classe O1 avec {0}", a);
+        }
+    }
+    class O2
+    {
+        public void M2(int a) { Console.WriteLine("Méthode M2 de la classe O2 avec {0}", a); }
+    }
+    class O3
+    {
+        public int M3() { return 0; }
+    }
+    delegate void D1(int a);
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var o1 = new O1();
+            var o2 = new O2();
+            var o = new O3();
+            var d1 = new D1(o1.M1);
+            var d2 = new D1(o2.M2);
+            o1.M1(10);
+            Console.WriteLine("Go");
+            d1.BeginInvoke(8, null, null);
+            Console.WriteLine("Fin");
+            d2.BeginInvoke(77, null, null);
+
+            Console.Read();
+        }
+    }
+}
---
+++ Sources/ConsoleApp6/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class PompeHydraulique
+    {
+        public void Refroidir(CentraleArgs args)
+        {
+            if (args.Boost)
+                Console.WriteLine("La pompe hydraulique est boostée ({0})", args.Temperature);
+            else 
+                Console.WriteLine("La pompe hydraulique est lancée ({0})", args.Temperature);
+        }
+    }
+    class PompeElectrique
+    {
+        public void Refroidir(CentraleArgs args) { Console.WriteLine("La pompe electrique est lancée ({0})", args.Pression); }
+    }
+    class PompeManuelle
+    {
+        public void Refroidir(CentraleArgs args) { Console.WriteLine("La pompe manuelle est lancée ({0})", args.Temperature); }
+    }
+    delegate void RefroidirDelegate(CentraleArgs args);
+    class CentraleArgs : EventArgs
+    {
+        public int Temperature;
+        public int Pression;
+        public bool Boost = false;
+
+    }
+    class Centrale
+    {
+        public event RefroidirDelegate FaitChaud;
+        //List<RefroidirDelegate> Delegues = new List<RefroidirDelegate>();
+
+        ArrayList Pompes = new ArrayList();
+        public Centrale()
+        {
+            var pompe1 = new PompeHydraulique();
+            var pompe2 = new PompeElectrique();
+            var pompe3 = new PompeElectrique();
+            var pompe4 = new PompeManuelle();
+            Pompes.Add(pompe1);
+            Pompes.Add(pompe2);
+            Pompes.Add(pompe3);
+
+            //Delegues.Add(new RefroidirDelegate(pompe1.Refroidir));
+            //Delegues.Add(new RefroidirDelegate(pompe2.Refroidir));
+            //Delegues.Add(new RefroidirDelegate(pompe3.Refroidir));
+            //Delegues.Add(new RefroidirDelegate(pompe4.Refroidir));
+            FaitChaud += new RefroidirDelegate(pompe1.Refroidir);
+            FaitChaud += new RefroidirDelegate(pompe2.Refroidir);
+            FaitChaud += new RefroidirDelegate(pompe3.Refroidir);
+            FaitChaud += new RefroidirDelegate(pompe4.Refroidir);
+
+        }
+        public void Refroidir()
+        {
+            //foreach(var pompe in Pompes)
+            //{
+            //    if (pompe is PompeHydraulique) ((PompeHydraulique)pompe).Refroidir();
+            //    else if (pompe is PompeElectrique) ((PompeElectrique)pompe).Refroidir();
+            //}
+
+            //foreach (var delegue in Delegues)
+            //{
+            //    delegue.Invoke();
+            //}
+
+            FaitChaud(new CentraleArgs { Pression = 30, Temperature = 3000, Boost = true });
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Centrale c1 = new Centrale();
+            c1.Refroidir();
+
+            Console.ReadLine();
+        }
+    }
+}
