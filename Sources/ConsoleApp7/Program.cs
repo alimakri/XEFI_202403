2ce1e41798db240ed8e116b5730b131f78ed98a2
Ali MAKRI <github@makri.fr>
2024-03-05 10:35:42 +0100

    10:35

---
+++ Sources/ConsoleApp7/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dessin d = null;
+
+
+            var original = new Dessin
+            {
+                Numero = Guid.NewGuid(),
+                Nom = "Guernica",
+                Auteur = "Picasso",
+                DatePublication = new DateTime(1935, 5, 20),
+                DateCopie = null
+            };
+            Dessin copie1 = original.Reproduire();
+            Dessin copie2 = original.Reproduire(DateTime.Now.AddDays(-1));
+            copie2.DateCopie = null;
+            Console.WriteLine(copie2.IsCopie());
+
+            var liste = original.Reproduire(10);
+            //var liste = Dessin.Reproduire(copie1, 10);
+            foreach(var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+            Console.Read();
+        }
+    }
+    class Dessin
+    {
+        private static Guid NumeroOriginal = default;
+        public Guid Numero;
+        public string Nom;
+        public string Auteur;
+        public DateTime DatePublication;
+        public DateTime? DateCopie;
+        public Dessin()
+        {
+            if (NumeroOriginal == default) NumeroOriginal = Guid.NewGuid();
+        }
+        public List<Dessin> Reproduire(int nCopie)
+        {
+            var list = new List<Dessin>();
+            for (int i = 0; i < nCopie; i++)
+            {
+                list.Add(Reproduire());
+                Thread.Sleep(1000);
+            }
+            return list;
+        }
+        public Dessin Reproduire()
+        {
+            return Reproduire(DateTime.Now);
+        }
+        public Dessin Reproduire(DateTime date)
+        {
+            return new Dessin
+            {
+                Numero = Guid.NewGuid(),
+                Nom = Nom,
+                Auteur = Auteur,
+                DatePublication = DatePublication,
+                DateCopie = date
+            };
+        }
+        public bool IsCopie()
+        {
+            return Numero != NumeroOriginal;
+        }
+        public override string ToString()
+        {
+            var copie = IsCopie() ? "(copie)" : "(Original)";
+            return Nom + " : " + Auteur + " " + copie + " " + DatePublication.ToString("dddd d MMM");
+        }
+    }
+}
---
+++ Sources/ConsoleApp8/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Démo 1
+            var kawa = new Moto { Id = Guid.NewGuid(), Nom = "Kawasaki", VitesseMax = 200 };
+            var v = 350;
+            ChangeVitesse(kawa, ref v);
+
+            // Démo 2
+            string s = "123"; int i = 0;
+            if (int.TryParse(s, out i))
+                Console.WriteLine(i);
+
+            // Démo 3
+            Moto[] tableauMotos = 
+            {
+                new Moto { Id = Guid.NewGuid(), Nom="Yamaha"},
+                new Moto { Id = Guid.NewGuid(), Nom="Honda"},
+                new Moto { Id = Guid.NewGuid(), Nom="Bmw"},
+            };
+            Affiche(tableauMotos);
+
+            // Démo 4
+            string maChainedeCaractere = "abc";
+            maChainedeCaractere += "def";
+            Console.WriteLine(maChainedeCaractere);
+
+            StringBuilder maChaine = new StringBuilder("abc");
+            maChaine.Append("def");
+            Console.WriteLine(maChaine);
+            
+            Console.ReadLine();
+        }
+
+        private static void Affiche(Moto[] tableauMotos)
+        {
+            foreach(var moto in tableauMotos)
+            {
+                Console.WriteLine(moto);
+            }
+        }
+
+        private static void ChangeVitesse(Moto moto, ref int nouvelleVitesse)
+        {
+            moto.VitesseMax = nouvelleVitesse;
+            nouvelleVitesse = 0;
+        }
+    }
+    class Moto
+    {
+        public Guid Id;
+        public string Nom;
+        public int VitesseMax;
+        public override string ToString()
+        {
+            return $"{Id}: Nom = {Nom}";
+        }
+    }
+}
