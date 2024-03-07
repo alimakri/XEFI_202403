c26867bdfa4ace5f17459b5c7705374c6daefbb0
Ali MAKRI <github@makri.fr>
2024-03-07 13:32:36 +0100

    13:32

---
+++ Sources/ZProject_TD/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZProject_TD
+{
+    public class Produit
+    {
+        public int Id;
+        public string Nom;
+        public decimal Prix;
+        public override string ToString()
+        {
+            return $"{Id}. {Nom} ({Prix})";
+        }
+    }
+}
--- Sources/ZProject_TD/Program.cs
+++ Sources/ZProject_TD/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -13,27 +15,68 @@ namespace ZProject_TD
     {
         static void Main(string[] args)
         {
-            var saisie = Console.ReadLine(); VerbeEnum verbe = VerbeEnum.None; NomEnum nom = NomEnum.None;
-
+            var saisie = Console.ReadLine(); 
+            VerbeEnum verbe = VerbeEnum.None; NomEnum nom = NomEnum.None;
+            List<Produit> liste = null;
             if (!Extraire(saisie, ref verbe, ref nom))
             {
-                Console.WriteLine("Instruction non valide"); 
+                Console.WriteLine("Instruction non valide");
             }
             else
             {
-                Requeter(verbe, nom);
+                liste = Requeter(verbe, nom);
             }
-
+            if (liste != null) Afficher(liste); else Console.WriteLine("Erreur !");
             Console.ReadLine();
         }
 
-        private static void Requeter(VerbeEnum verbe, NomEnum nom)
+        private static void Afficher(List<Produit> liste)
         {
-
+            foreach (Produit p in liste)
+            {
+                Console.WriteLine(p);
+            }
         }
-        private static void GetProduct()
-        {
 
+        private static List<Produit> Requeter(VerbeEnum verbe, NomEnum nom)
+        {
+            if (verbe == VerbeEnum.Get && nom == NomEnum.Product)
+            {
+                return GetProduct();
+            }
+            return null;
+        }
+        private static List<Produit> GetProduct()
+        {
+            var liste = new List<Produit>(); SqlDataReader rd;
+            // Connexion
+            SqlConnection cnx = new SqlConnection();
+            cnx.ConnectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=AdventureWorks2017;Integrated Security=true";
+            try
+            {
+                cnx.Open();
+            }
+            catch (Exception ex) { return null; }
+            // Requete
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = cnx;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = @"select ProductID, Name, ListPrice from Production.Product";
+
+            try
+            {
+                rd = cmd.ExecuteReader();
+            }
+            catch (Exception ex) { return null; }
+
+
+            // Affichage
+            while (rd.Read())
+            {
+                liste.Add(new Produit { Id = (int)rd["ProductID"], Nom = rd["Name"].ToString(), Prix = (decimal)rd["ListPrice"] });
+            }
+            rd.Close();
+            return liste;
         }
 
         private static bool Extraire(string saisie, ref VerbeEnum verbe, ref NomEnum nom)
