4669afd643917500d639dc6f7975e8e33f432a06
Ali MAKRI <github@makri.fr>
2024-03-07 10:00:30 +0100

    10:00

---
+++ Sources/ZProject_TD/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ZProject_TD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var saisie = Console.ReadLine();
+            Regex reg = new Regex("([A-Z][a-z0-9]+)-([A-Z][a-z0-9]+)");
+
+            Console.ReadLine();
+        }
+    }
+}
